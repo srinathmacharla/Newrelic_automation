10aabc9655d5f4e42430073d88f807541af69699
srinathmacharla <153605611+srinathmacharla@users.noreply.github.com>
2024-04-15 21:14:53 +0530

    Update main.js

--- src/newrelic_typescript/main.js
+++ src/newrelic_typescript/main.js
@@ -5,7 +5,7 @@ var yaml = require("js-yaml");
 var path = require("path");
 var dashboardPath = '../newrelic_terraform/dashboard.json';
 var yamlFolderPath = '../dashboard-yaml';
-var yamlPath = '../resources/file.yaml';
+var yamlPath = 'http://localhost:8080/job/newrelic';
 var envPath = '../resources/env_variable.yaml';
 function replaceServiceAppNameInQueries(yamlData, envData) {
     var regex = /\$\{service\.(\w+)\}/g;
